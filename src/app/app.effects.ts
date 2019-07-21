6c7deef7e089bd23248e17e68cdbd3e95f514df9
Souvik93 <souvik.das@live.in>
2019-07-21 21:34:57 +0530

    1 file modified, 1 added

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -3,14 +3,17 @@ import { Effect , Actions , ofType } from '@ngrx/effects';
 import { Store , select } from '@ngrx/store';
 import { AppService } from './app.service';
 import { MovieState } from './app.state';
-import { GetMovieDetails , GetMovieDetailsSuccess } from './app.actions';
+import { GetMovieDetails , GetMovieDetailsSuccess , MovieDetailsActions } from './app.actions';
+import { of } from 'rxjs';
+import { switchMap  } from 'rxjs/operators';
 
 @Injectable()
 export class AppEffects {
   @Effect()
   getMovirs$ = this.action.pipe(
-    ofType<GetMovieDetails>()
-  )
+    ofType<GetMovieDetails>(MovieDetailsActions.GetMovieDetails),
+    switchMap(()=> new GetMovieDetailsSuccess(this.appService.getMovies()) )
+  );
 
   constructor(private appService: AppService, private action: Actions, private store: Store<MovieState> ) {
 
